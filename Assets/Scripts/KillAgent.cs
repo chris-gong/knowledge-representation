351b3dee92bec1e32c97921bbf1c628b65688a11
chris-gong <chris_gong@yahoo.com>
2018-10-11 19:07:31 -0400

    made a player object that can destroy agents on collision

---
+++ Assets/Scripts/KillAgent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillAgent : MonoBehaviour
+{
+
+    // Use this for initialization
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Agent")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour {
+    public float moveSpeed;
+    public Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 translation = new Vector3(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0, moveSpeed* Input.GetAxis("Vertical") * Time.deltaTime);
+        rb.MovePosition(transform.position + translation);
+  
+	}
+}
