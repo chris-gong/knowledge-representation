450f237b0516d44e2fd53e78e28ed63c6c9937a3
Bailey Chen <bchen919@gmail.com>
2018-11-01 00:52:22 -0400

    clue class

---
+++ Assets/Scripts/Clue.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocationClue {
+
+    private int agentID;
+    private int zoneID;
+    private int timeInt;
+    
+
+
+    public LocationClue(int agent, int zone, int time)
+    {
+        this.agentID = agent;
+        this.zoneID = zone;
+        this.timeInt = time;
+    }
+
+    public static int CmpTime(LocationClue obj, LocationClue other)
+    {
+        return obj.timeInt - other.timeInt;
+    }
+        
+}
+
+public class MurderItemClue
+{
+    private int agentID;
+    private string itemName;
+    private int timeInt;
+
+
+}
