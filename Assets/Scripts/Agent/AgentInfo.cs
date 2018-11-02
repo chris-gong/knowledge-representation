569b7c821994ccbecfadf4de8006b1b8211c30ef
kcolyhs <bchen919@gmail.com>
2018-11-01 22:56:30 -0400

    Created agentinfo but still need to generate prefab on time update

--- Assets/Scripts/Agent/AgentInfo.cs
+++ Assets/Scripts/Agent/AgentInfo.cs
@@ -5,8 +5,26 @@ using UnityEngine;
 public class AgentInfo : MonoBehaviour {
 
     public int agentId;
-    public string agentName;
+    public string agentName = null;
+    private GameObject currentObs = null;
+    public GameObject blankObs;
 
-    
+    public void InitAgentInfo(int newID, string name){
+        blankObs = Resources.Load<GameObject>("Assets/Prefabs/Observable.prefab");
+        if (agentName == null)
+        {
+            agentName = string.Format("Agent#{0}", newID);
+        }
+        agentId = newID;
+        GameController.GetInstanceTimeController().onTimeTick.AddListener(UpdateAgentObs);
+    }
 
+    public void UpdateAgentObs(){
+        if(!currentObs){
+            Destroy(currentObs);
+        }
+        GameObject newobs = Instantiate(blankObs,gameObject.transform);
+        Observable obsInfo= newobs.GetComponent<Observable>();
+        // TODO Add up-to-date information on the agent's current location
+    }
 }
