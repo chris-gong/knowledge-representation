d0fba83f6fe8054063d0ba966dc2973bed38349b
chris-gong <chris_gong@yahoo.com>
2018-11-30 11:01:42 -0500

    added flashing lights in response to murder

---
+++ Assets/Scripts/Controller/LightController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour {
+
+    private Color originalColor;
+    public Color dangerColor;
+    public int flickerDuration;
+    private bool flickered = false;
+    private Light lights;
+	// Use this for initialization
+	void Start () {
+        lights = GetComponent<Light>();
+        originalColor = lights.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //lights only flicker once per round when the murder occurs
+		if(!flickered && GameController.GetInstanceTimeController().GetMurderTime() > -1)
+        {
+            flickered = true;
+            StartCoroutine(FlickerLights());
+        }
+	}
+
+    IEnumerator FlickerLights()
+    {
+        lights.color = dangerColor;
+        lights.intensity = 0;
+        for (int i = 0; i < flickerDuration; i++)
+        {
+            yield return new WaitForSeconds(1);
+            lights.intensity = 1;
+            yield return new WaitForSeconds(1);
+            lights.intensity = 0;
+        }
+        lights.color = originalColor;
+        lights.intensity = 1;
+    }
+}
---
+++ Assets/Scripts/InventoryButton.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryButton : MonoBehaviour {
+
+    public Item item;
+    public int index = 0;
+    private Image image;
+    private Text text;
+    private Button button;
+    public void InitButton(int indexArg)
+    {
+        index = indexArg;
+        image = gameObject.GetComponent<Image>();
+        text = transform.GetChild(0).gameObject.GetComponent<Text>();
+        button = gameObject.GetComponent<Button>();
+        button.onClick.AddListener(this.UseItem);
+    }
+
+    public void UseItem()
+    {
+        Debug.Log("UseItem() invoked!");
+        item.OnUse();
+        if(item.consumable){
+            GameController.GetInstanceInventoryController().RemoveItem(item);
+        }
+    }
+
+    public void UpdateItem(Item newItem)
+    {
+        if (newItem == null) {
+            EnableButton(false);
+            return;
+        }
+        else {
+            EnableButton(true);
+        }
+        item = newItem;
+        text.text = item.name;
+    }
+
+    void EnableButton(bool val)
+    {
+        button.enabled = val;
+        text.enabled = val;
+        image.enabled = val;
+    }
+
+    
+
+}
